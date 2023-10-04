4b97a01d13852f0e05e15916bddadaa4eaa339ad
Electromorphous <pstation.474@gmail.com>
2023-10-05 00:25:31 +0530

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: {},
+  define: { global: {} },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+    },
   },
 });
